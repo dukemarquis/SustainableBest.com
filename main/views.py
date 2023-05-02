ad2d2feedf25fef9d9fbf1b5d63a5e8afbef7d7b
hancao <1787873794@qq.com>
2023-05-02 11:02:53 +0800

    CSFR Protect

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.views.decorators.csrf import csrf_protect
 
 from .models import Sustainable_Products
 
@@ -24,7 +25,7 @@ class RecommendResultView(generic.DetailView):
 class SusProductDetailView(generic.DetailView):
     model = Sustainable_Products
 
-
+@csrf_protect
 def recommend(request):
     sustainable_product = Sustainable_Products.objects.create(
                                                             product_image = request.FILES['sus_product_photo'],
